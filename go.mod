d4267199b9ffe31ed0c30f0b6796efd5c9ac5f25
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-21 16:38:05 +0900

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/qj0r9j0vc2/average-block-interval-check
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+package main
+
+import (
+"encoding/json"
+"fmt"
+"io/ioutil"
+"net/http"
+"os"
+"strconv"
+"time"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run main.go [height] [iterations]")
+		os.Exit(1)
+	}
+	height, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Invalid height:", os.Args[1])
+		os.Exit(1)
+	}
+	iterations, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Invalid iterations:", os.Args[2])
+		os.Exit(1)
+	}
+
+	var totalDiff uint64
+
+	for i := 0; i < iterations; i++ {
+		blockTime, err := getBlockTime(height)
+		if err != nil {
+			fmt.Println("Failed to get block time:", err)
+			os.Exit(1)
+		}
+
+		prevBlockTime, err := getBlockTime(height - 1)
+		if err != nil {
+			fmt.Println("Failed to get previous block time:", err)
+			os.Exit(1)
+		}
+
+		diff := blockTime - prevBlockTime
+
+		totalDiff += diff
+
+		height--
+	}
+
+	average := float64(totalDiff) / float64(iterations)
+
+	fmt.Printf("Average time difference: %.4f seconds\n", average)
+}
+
+func getBlockTime(height int) (uint64, error) {
+	url := fmt.Sprintf("http://localhost:26657/block?height=%d", height)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	var result map[string]interface{}
+	if err := json.Unmarshal(body, &result); err != nil {
+		return 0, err
+	}
+	blockHeader := result["result"].(map[string]interface{})["block"].(map[string]interface{})["header"].(map[string]interface{})
+	blockTimeStr := blockHeader["time"].(string)
+
+	blockTime, err := time.Parse(time.RFC3339Nano, blockTimeStr)
+	if err != nil {
+		return 0, err
+	}
+	return blockTime.Unix(), nil
+}
